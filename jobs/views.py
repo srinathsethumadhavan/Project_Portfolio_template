8af85806e35c16130acf92affd16881037d8d301
srinathsethumadhavan <srinath15021997@gmail.com>
2020-03-22 20:30:41 +0530

    project portfolio app

---
+++ jobs/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Job
+# Create your views here.
+
+def srinath(request):
+    jobs = Job.objects
+    return render(request,'job/home.html',{'jobs':jobs})
+
+
+def detail(request, job_id):
+    print(job_id)
+    # print(21)
+    job_detail= get_object_or_404(Job, pk=job_id)
+    return render(request,'job/detail.html',{'job':job_detail})
